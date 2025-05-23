a856c4c8f235d778ee268e6c58c616c6b7a845cf
june <junezqj@gmail.com>
2025-05-23 17:39:59 +0800

    refactor：界面重构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,46 +1,43 @@
-import { defaultTheme } from '@vuepress/theme-default'
-import { viteBundler } from '@vuepress/bundler-vite'
+import {defaultTheme} from '@vuepress/theme-default'
+import {viteBundler} from '@vuepress/bundler-vite'
 
 export default {
     theme: defaultTheme({
         logo: '/images/profile-pic.png',
-        sidebar: [
-            {
-                text: '首页',
-                link: '/',
-            },
-            {
-                text: 'GIT 版本控制',
-                children: [
-                    {
-                        text: '基本命令',
-                        link: '/git/command',
-                    },
-                ],
-            },
-            {
-                text: 'CSS 样式',
-                children: [
-                    {
-                        text: 'sass & less & stylus',
-                        link: '/css/sass',
-                    },
-                ],
-            },
-            {
-                text: 'JavaScript 语法',
-                children: [
-                    {
-                        text: '闭包',
-                        link: '/js/closure',
-                    },
-                ],
-            },
+        navbar: [
+            {text: '首页', link: '/'},
+            {text: '前端', link: '/frontend/'},
+            // {text: '后端', link: '/backend'},
         ],
+        sidebar: {
+            '/frontend/': [
+                {
+                    text: 'GIT 版本控制',
+                    children: [
+                        { text: '基本命令', link: '/frontend/git/command' },
+                    ],
+                },
+                {
+                    text: 'CSS 样式',
+                    children: [
+                        { text: 'sass & less & stylus', link: '/frontend/css/sass' },
+                    ],
+                },
+                {
+                    text: 'JavaScript 语法',
+                    children: [
+                        { text: '闭包', link: '/frontend/js/closure' },
+                    ],
+                },
+            ],
+            '/': [ // 主页侧边栏
+                { text: '首页', link: '/' }
+            ],
+        }
     }),
     bundler: viteBundler(),
     lang: 'zh-CN',
     title: "Bala bala's website",
     description: '这是我的一个技术分享&个人博客网站',
-    head: [['link', { rel: 'icon', href: '/images/profile-pic.png' }]],
+    head: [['link', {rel: 'icon', href: '/images/profile-pic.png'}]],
 }
